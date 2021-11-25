785dad8499b854f08dae5315eff854f8cd79375e
mbugua <mbuguamwaura9@gmail.com>
2021-11-25 12:05:55 +0300

    attempt to fix the banner image

--- src/components/BannerComponent.js
+++ src/components/BannerComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Image from 'react-bootstrap/Image'
+import banner from '../data/banner'
 import './navbar.css'
 
 class BannerComponent extends Component {
@@ -8,7 +9,7 @@ class BannerComponent extends Component {
         return (
             <div className="contain">
 
-                <Image src="sagret_gardens_banner.jpeg"  style={{width:"-webkit-fill-available",maxWidth:"100%",objectFit:"cover",height:"450px",filter:"brightness(0.65)"}} />
+                <Image src={banner.icon}  style={{width:"-webkit-fill-available",maxWidth:"100%",objectFit:"cover",height:"450px",filter:"brightness(0.65)"}} />
                 <div className="center title">Sagret Gardens</div>
             
             </div>
---
+++ src/data/banner.js
@@ -0,0 +1,4 @@
+const icons = {
+    icon:"sagret_gardens_banner.jpeg"
+}
+export default icons
