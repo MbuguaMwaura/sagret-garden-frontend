f1649382415b6b392588dfc60acc94a3416ebb49
mbugua <mbuguamwaura9@gmail.com>
2022-01-07 18:32:48 +0300

    makes banner image better on large screens

--- src/components/BannerComponent.js
+++ src/components/BannerComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Image from 'react-bootstrap/Image'
 import banner from '../data/banner'
 import './navbar.css'
+import './large.css'
 
 class BannerComponent extends Component {
 
@@ -9,7 +10,7 @@ class BannerComponent extends Component {
         return (
             <div className="contain">
 
-                <Image src="sagret_gardens_banner.jpg"  style={{width:"-webkit-fill-available",maxWidth:"100%",objectFit:"cover",height:"350px"}} />
+                <Image className="banner_image" src="sagret_gardens_banner.jpg"  style={{width:"-webkit-fill-available",maxWidth:"100%",objectFit:"cover"}} />
           
             
             </div>
